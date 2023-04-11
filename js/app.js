75a951a399683c9c3a0f41b331495accc6865a7e
Laith-Vlad <laithxfg13@gmail.com>
2023-04-11 15:08:48 +0000

    todo-js2

--- js/app.js
+++ js/app.js
@@ -37,3 +37,27 @@ if (
   }
 
 }
+
+let quastionA = prompt('Are you will ?','yes/no')
+let quastionB = prompt('Are you tired of working?','yes/no')
+let quastionC = prompt('Are you enjoying the last days of winter?','yes/no')
+
+quastionA.toLowerCase()
+quastionB.toLowerCase()
+quastionC.toLowerCase()
+
+
+
+let answers = [quastionA, quastionB, quastionC]
+
+
+for (let i = 0; i < answers.length; i++) {
+  if (answers[i] == ['']) {
+  answers[i] = ['invalid']
+
+}
+
+}
+
+
+console.log(answers)
